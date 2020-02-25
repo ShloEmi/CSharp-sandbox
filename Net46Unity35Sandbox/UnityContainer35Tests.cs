cf552cf816aab4320612873b2643f1cf74d44a41
shlomio <shlomio@camtek.com>
2020-02-25 08:24:23 +0200

    +Net46Unity35Sandbox of ++ Test_CreateChildContainer_ChildAddsInstance_ExpectingChildCanResolveParentsRegistration ++ Test_CreateChildContainer_ChildRegisterInstance_ExpectingParentCanNotResolveChildRegistration

--- Net46Unity35Sandbox/UnityContainer35Tests.cs
+++ Net46Unity35Sandbox/UnityContainer35Tests.cs
@@ -31,67 +31,41 @@ namespace Net46Unity35Sandbox
         }
 
         [Test]
-        public void Test_CreateChildContainer_ChildAddsInstance_ExpectingParentSeeThatInstanceAndCanUseIt()
+        public void Test_CreateChildContainer_ChildAddsInstance_ExpectingChildCanResolveParentsRegistration()
         {
             var parentContainer = new UnityContainer();
             var i1Mock = new Mock<II1>();
             i1Mock.Setup(i1 => i1.f1(1)).Returns(42);
             parentContainer.RegisterInstance(i1Mock.Object);
 
-            var i2Mock = new Mock<II2>();
-            i2Mock.Setup(i1 => i1.f2(1)).Returns(24);
-
             //parent
             var i1Obj = parentContainer.Resolve<II1>();
-            Assert.Throws<ResolutionFailedException>(() => parentContainer.Resolve<II2>());
             Assert.IsNotNull(i1Obj);
             Assert.AreEqual(0, i1Obj.f1(0));
             Assert.AreEqual(42, i1Obj.f1(1));
 
             //child
             IUnityContainer childContainer = parentContainer.CreateChildContainer();
-            parentContainer.RegisterInstance(i2Mock.Object);
 
             i1Obj = childContainer.Resolve<II1>();
             Assert.IsNotNull(childContainer.Resolve<II1>());
-            Assert.IsNotNull(childContainer.Resolve<II2>());
             Assert.AreEqual(0, i1Obj.f1(0));
             Assert.AreEqual(42, i1Obj.f1(1));
 
             //parent
-            Assert.DoesNotThrow(() => parentContainer.Resolve<II2>());
-            childContainer.Dispose();
-            childContainer = null;
-
-            Assert.DoesNotThrow(() => parentContainer.Resolve<II2>());
         }
-        [Test]
 
-        public void Test_CreateChildContainer_ChildAddsInstance_ExpectingParentSeeThatInstanceAndCanNotUseIt()
+        [Test]
+        public void Test_CreateChildContainer_ChildRegisterInstance_ExpectingParentCanNotResolveChildRegistration()
         {
             var parentContainer = new UnityContainer();
-            var i1Mock = new Mock<II1>();
-            i1Mock.Setup(i1 => i1.f1(1)).Returns(42);
-            parentContainer.RegisterInstance(i1Mock.Object);
-
-            //parent
-            var i1Obj = parentContainer.Resolve<II1>();
-            Assert.Throws<ResolutionFailedException>(() => parentContainer.Resolve<II2>());
-            Assert.IsNotNull(i1Obj);
-            Assert.AreEqual(0, i1Obj.f1(0));
-            Assert.AreEqual(42, i1Obj.f1(1));
+            IUnityContainer childContainer = parentContainer.CreateChildContainer();
 
             var i2Mock = new Mock<II2>();
             i2Mock.Setup(i1 => i1.f2(1)).Returns(24);
-            //child
-            IUnityContainer childContainer = parentContainer.CreateChildContainer();
-            childContainer.RegisterInstance(i2Mock.Object/*, new HierarchicalLifetimeManager()*/);
+            childContainer.RegisterInstance(i2Mock.Object);
 
-            i1Obj = childContainer.Resolve<II1>();
-            Assert.IsNotNull(childContainer.Resolve<II1>());
             Assert.IsNotNull(childContainer.Resolve<II2>());
-            Assert.AreEqual(0, i1Obj.f1(0));
-            Assert.AreEqual(42, i1Obj.f1(1));
 
             //parent
             Assert.Throws<ResolutionFailedException>(() => parentContainer.Resolve<II2>());
